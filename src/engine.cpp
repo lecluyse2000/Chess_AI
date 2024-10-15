0b0c3b2f23d1c7a8b9ce1eede9f229521b358422
Caden LeCluyse <lecluyse20@gmail.com>
2024-10-15 17:51:21 -0500

    Added a function to get all knight moves

--- src/engine.cpp
+++ src/engine.cpp
@@ -2,12 +2,43 @@
 
 #include "engine.h"
 
+#include <array>
+#include <bit>
+#include <cassert>
+#include <cstdint>
 #include "gamestate.h"
 #include <optional>
 #include "ui.h"
+#include "util.h"
+
 
 namespace Engine
 {
+
+// @Param bitboard: A valid bitboard with at least one knight on it 
+// @Return: A bitboard with all available knight moves
+
+[[nodiscard]] static constexpr std::uint64_t get_kight_moves(const std::uint64_t bitboard)
+{
+    const std::uint64_t num_knights = std::popcount(bitboard);
+    assert(num_knights == 1 || num_knights == 2);
+    std::uint64_t move_board = 0;
+    std::array<std::uint64_t, 2> knight_positions = {Util::get_least_sig_bit(bitboard), 0};
+
+    if (num_knights == 2) {
+        knight_positions[1] = Util::clear_least_sig_bit(bitboard);
+    }
+
+    for (const auto pos : knight_positions) {
+        if (!pos) return move_board;
+
+        for (const auto bit_shift : knight_bit_shifts) {
+            move_board |= (pos << bit_shift) | (pos >> bit_shift);
+        }
+    }
+
+    return move_board;
+}
 
 static inline void game_mode()
 {
--- src/engine.h
+++ src/engine.h
@@ -3,8 +3,12 @@
 #ifndef ENGINE_H
 #define ENGINE_H
 
+#include <array>
+#include <cstdint>
+
 namespace Engine
 {
+    inline constexpr std::array<std::uint64_t, 4> knight_bit_shifts = {6, 15, 17, 10};
     [[nodiscard]] int run(const bool analysis_mode);
 }
 
