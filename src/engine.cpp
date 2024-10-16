3378c4aac42a61f0e37b3d264e87cae9fbfdf372
Caden LeCluyse <lecluyse20@gmail.com>
2024-10-16 00:21:07 -0500

    Lookup table done for knights

--- src/engine.cpp
+++ src/engine.cpp
@@ -2,43 +2,14 @@
 
 #include "engine.h"
 
-#include <array>
-#include <bit>
 #include <cassert>
-#include <cstdint>
 #include "gamestate.h"
 #include <optional>
 #include "ui.h"
-#include "util.h"
-#include <iostream>
 
 
 namespace Engine
 {
-
-// @Param bitboard: A valid bitboard with at least one knight on it 
-// @Return: A bitboard with all available knight moves
-
-[[nodiscard]] static constexpr std::uint64_t get_kight_moves(const std::uint64_t bitboard)
-{
-    const std::uint64_t num_knights = std::popcount(bitboard);
-    assert(num_knights == 1 || num_knights == 2);
-    std::uint64_t move_board = 0;
-    const std::array<const std::uint64_t, 2> knight_positions = {Util::get_least_sig_bit(bitboard),
-                                                                (num_knights == 2) ? Util::clear_least_sig_bit(bitboard) : 0};
-
-    for (const auto pos : knight_positions) {
-        if (!pos) return move_board;
-
-        // I hope with inlining and optimizations all this will be properly optimized
-        move_board |= (Util::shift_left(pos) | Util::shift_right(pos)) << 16;
-        move_board |= (Util::shift_left(pos) | Util::shift_right(pos)) >> 16;
-        move_board |= (Util::shift_left(Util::shift_left(pos)) | Util::shift_right(Util::shift_right(pos))) << 8;
-        move_board |= (Util::shift_left(Util::shift_left(pos)) | Util::shift_right(Util::shift_right(pos))) >> 8;
-    }
-
-    return move_board;
-}
 
 static inline void game_mode()
 {
--- src/engine.h
+++ src/engine.h
@@ -4,11 +4,45 @@
 #define ENGINE_H
 
 #include <array>
+#include <bit>
 #include <cstdint>
+#include "util/util.h"
 
 namespace Engine
 {
-    [[nodiscard]] int run(const bool analysis_mode);
+
+// @Param bitboard: A valid bitboard with at least one knight on it 
+// @Return: A bitboard with all available knight moves
+
+[[nodiscard]] static consteval std::uint64_t get_knight_moves(const std::uint64_t pos)
+{
+    std::uint64_t move_board = 0;
+
+    move_board = (Util::shift_left(pos) | Util::shift_right(pos)) << 16;
+    move_board |= (Util::shift_left(pos) | Util::shift_right(pos)) >> 16;
+    move_board |= (Util::shift_left(Util::shift_left(pos)) | Util::shift_right(Util::shift_right(pos))) << 8;
+    move_board |= (Util::shift_left(Util::shift_left(pos)) | Util::shift_right(Util::shift_right(pos))) >> 8;
+
+    return move_board;
+}
+
+// @Desc: Compile time function to generate the lookup table for knights
+// @Return: The lookup table
+
+[[nodiscard]] consteval std::array<std::uint64_t, 64> generate_knight_lookup_table()
+{
+    std::array<std::uint64_t, 64> table;
+    for (int i = 0; i < 64; ++i) {
+        table[i] = get_knight_moves(1ULL << i); 
+    }
+
+    return table;
+}
+
+inline constexpr auto knight_table = generate_knight_lookup_table();
+
+[[nodiscard]] int run(const bool analysis_mode);
+
 }
 
 #endif
